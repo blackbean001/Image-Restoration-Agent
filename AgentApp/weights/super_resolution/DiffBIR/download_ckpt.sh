5844b93e026dc86a7728c067be7c70f83fc41cd0
blackbean001 <773675806@qq.com>
2025-11-14 16:28:11 -0400

    update download_ckpt.sh for diffbir

--- AgentApp/weights/super_resolution/DiffBIR/download_ckpt.sh
+++ AgentApp/weights/super_resolution/DiffBIR/download_ckpt.sh
@@ -3,4 +3,5 @@ wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/v2.pth
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/v1_general.pth
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/v1_face.pth
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/codeformer_swinir.ckpt
+wget https://huggingface.co/mykh-med/face-enchacer/resolve/main/general_swinir_v1.ckpt
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/realesrgan_s4_swinir_100k.pth
