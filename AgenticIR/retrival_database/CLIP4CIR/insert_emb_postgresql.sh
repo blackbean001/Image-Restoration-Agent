de14bb80b85b3b5238dc1fccc18d61dac2f8a21d
jasonsong.li <Jasonsong.Li@sunia.com>
2025-11-19 12:07:35 -0800

    update CLIP4CIR inserting to postgresql

--- AgenticIR/retrival_database/CLIP4CIR/insert_emb_postgresql.sh
+++ AgenticIR/retrival_database/CLIP4CIR/insert_emb_postgresql.sh
@@ -1,9 +1,9 @@
 CUDA_VISIBLE_DEVICE=0
 python -m src.insert_emb_to_postgresql \
-  --imgs-dir "/home/jason/AgenticIR/output"  \
-  --combiner-path "/home/jason/CLIP4Cir/models/combiner_trained_on_imgres_RN50x4_2025-09-05_12:30:03/saved_models/combiner_arithmetic.pt"  \
+  --imgs-dir "../../../AgenticIR/output"  \
+  --combiner-path "./CLIP4Cir/models/combiner_trained_on_imgres_RN50x4/saved_models/combiner_arithmetic.pt"  \
   --combining-function 'combiner' \
-  --clip-model-path "/home/jason/CLIP4Cir/models/clip_finetuned_on_imgres_RN50x4_2025-09-05_10:48:31/saved_models/tuned_clip_arithmetic.pt"  \
+  --clip-model-path "./CLIP4Cir/models/clip_finetuned_on_imgres_RN50x4/saved_models/tuned_clip_arithmetic.pt"  \
   --projection-dim 2560 \
   --hidden-dim 5120 \
   --transform "targetpad"  \
