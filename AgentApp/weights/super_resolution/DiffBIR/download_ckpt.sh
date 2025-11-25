2ab54a2a4916472e555f2ef01d50f61a397262cb
jasonsong.li <Jasonsong.Li@sunia.com>
2025-11-25 15:20:23 -0800

    update and debug

--- AgentApp/weights/super_resolution/DiffBIR/download_ckpt.sh
+++ AgentApp/weights/super_resolution/DiffBIR/download_ckpt.sh
@@ -1,6 +1,7 @@
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/DiffBIR_v2.1.pt
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/v2.pth
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/v1_general.pth
+wget https://huggingface.co/lxq007/DiffBIR/resolve/main/general_full_v1.ckpt
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/v1_face.pth
 wget https://huggingface.co/lxq007/DiffBIR-v2/resolve/main/codeformer_swinir.ckpt
 wget https://huggingface.co/mykh-med/face-enchacer/resolve/main/general_swinir_v1.ckpt
